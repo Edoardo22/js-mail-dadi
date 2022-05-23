d26a176764bb53ad52116723a8ae6698f854c84d
Edoardo22 <vanaliedoardo@gmail.com>
2022-05-23 16:25:39 +0200

    manca bonus

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,4 +1,18 @@
-let numeriRandomElement = document.getElementById("numeriRandom")
-let numeriLista = [""]
+let numeriRandomElement = document.getElementById("numeriRandom");
+let numeriLista = [""];
+let submitElement = document.getElementById("submit");
+let nN = 0;
 
-numeriRandomElement.addEventListener()
+submitElement.addEventListener("click", function () {
+
+    console.log(numeriRandomElement.value);
+    if (numeriRandomElement.value === undefined || NaN) {
+        alert("inserisci un numero")
+    }
+    for (i = 0; i = numeriRandomElement.value; i++) {
+        nN = Math.random()
+        numeriLista.push(nN)
+    }
+})
+
+
